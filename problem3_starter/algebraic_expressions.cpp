60325317ba1eb50c672018e24d3800c6cc384b63
Quentin Wilson <qaw1@pitt.edu>
2025-02-11 18:14:00 -0500

    finished prob 3 code

--- problem3_starter/algebraic_expressions.cpp
+++ problem3_starter/algebraic_expressions.cpp
@@ -51,7 +51,84 @@ bool isPost(std::string s)
   return (firstChar == 0);
 }
 
+
+
+//below is the TODO of the convert functionality
+
+
+
+
 void convert(const std::string &postfix, std::string &prefix)
 {
-  // TODO
+  //checking if the given post is valid if not it throws an invalid
+  //argument error as seen below
+  if(!isPost(postfix)){
+    throw std::invalid_argument("Invalid postfix expression");
+
+  }
+
+  //this is a testing if the expression contains one operator
+  if(postfix.size()==1){
+    //checking to make sure the operator is valid (the character is the operand)
+    if(isalpha(postfix[0])){
+      //assigning th op to prefix so there is not conversion needed yet
+
+      prefix =postfix;
+      //empty return to get out of the loop
+      return;
+
+    }
+    else{
+      //throws an invalid argument error if an op is not found in the expression
+
+      throw std::invalid_argument("invalid expression");
+
+    }
+  }
+
+  //recursion here- this converts the expression
+  char lastChar = postfix[postfix.size()-1];
+  // this if statement checks if the last char
+  //is an op. if it is not, the invalid expression error will be thrown
+  //to indicate
+  if (!isoperator(lastChar)){
+    throw std::invalid_argument("invalid expression");
+
+  }
+  //finding if there is a second operand in the expression
+  int lastOperand = endPost(postfix,postfix.size()-2);
+  //if there is not an operand found (-1 used in this case cause if there were 
+  //2 operands it would return 0 which would not be correct so -1 is used)
+  //there will be an invalid argument error thrown to indicate that there was not more than
+  //1 op in the expression given
+
+  if(lastOperand==-1){
+    throw std::invalid_argument("invalid expression");
+  }
+
+
+  //this splits the post fix into two parts (left and right) 0-last then last-post -last-1 to correctly size
+  //in order to achieve the split correctly
+  //this is the left substring and the right substring respectively 
+  std::string leftpostfix=postfix.substr(0,lastOperand);
+  std::string rightpostfix = postfix.substr(lastOperand,postfix.size()-lastOperand-1);
+
+
+  //declared left and right prefixes which will be used to convert the vonverted
+  //prefix expressios from the previous lines of code
+
+  std::string leftprefix;
+  std::string rightprefix;
+
+  //now converting the expressions from post to pre notation
+  convert(leftpostfix,leftprefix);
+  convert(rightpostfix,rightprefix);
+
+
+  //finally setting everything back into one whole expression in the 
+  //prefix format which is the operator , left fix, and right fix
+  
+  prefix=lastChar+leftprefix+rightprefix;
+  
+
 }
--- problem3_starter/test_postpre.cpp
+++ problem3_starter/test_postpre.cpp
@@ -60,3 +60,52 @@ TEST_CASE("Postpre: Test other operators", "[postpre]")
 }
 
 /* TODO: Your test cases here */
+
+
+TEST_CASE("testing end post function again with different exp","[postpre]"){
+  std::string postfix_valid="abc*+";
+  int test_valid=0;
+  int test_invalid=-1;
+  std::string postfix_invalid= "a+";
+  REQUIRE(endPost(postfix_valid,postfix_valid.length()-1)==test_valid);
+  REQUIRE(endPost(postfix_invalid,postfix_invalid.length()-1)==test_invalid);
+
+}
+
+TEST_CASE("converting long expressions","[postpre]"){
+  std::string postfix = "mnop*-q+";
+  std::string prefix = "";
+  std::string test = "+-*mnopq";
+
+  REQUIRE_NOTHROW(convert(postfix, prefix));
+  REQUIRE(prefix == test);
+}
+
+TEST_CASE("testing multiple operators","[postpre]"){
+
+  std::string postfix = "uv+wx-*";
+  std::string prefix = "";
+  std::string test = "-*+uvwx";
+
+  REQUIRE_NOTHROW(convert(postfix, prefix));
+  REQUIRE(prefix == test);
+}
+
+
+TEST_CASE("testing mixed operations","[postpre]"){
+  std::string postfix = "rs+tu*/v-";
+  std::string prefix = "";
+  std::string test = "-/*+rstuv";
+
+  REQUIRE_NOTHROW(convert(postfix, prefix));
+  REQUIRE(prefix == test);
+}
+
+TEST_CASE("invalid expressions test","postpre"){
+  std::string postfix_invalid1 = "x+y";
+  std::string prefix;
+  REQUIRE_THROWS_AS(convert(postfix_invalid1, prefix), std::invalid_argument);
+
+  std::string postfix_invalid2 = "-yz";
+  REQUIRE_THROWS_AS(convert(postfix_invalid2, prefix), std::invalid_argument);
+}
